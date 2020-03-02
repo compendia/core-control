c295292cbcbbe73a556613e9bc12424ef2f54e20
Dean van Dugteren <hello@dean.press>
2020-03-02 23:09:22 +0100

    chore: force update on install_deps

--- functions.sh
+++ functions.sh
@@ -249,8 +249,8 @@ status() {
 }
 
 install_deps () {
-  sudo apt-get update
-  sudo apt-get upgrade
+  sudo apt-get update -y
+  sudo apt-get upgrade -y
   sudo timedatectl set-ntp no > /dev/null 2>&1
   sudo apt install -y htop curl build-essential python git nodejs npm libpq-dev ntp gawk jq > /dev/null 2>&1
   sudo npm install -g n grunt-cli pm2@3 yarn lerna > /dev/null 2>&1
