bd664dca32743417ff0de1c246616247df556980
Dean van Dugteren <hello@dean.press>
2020-08-21 21:34:29 +0200

    chore: fix status command

--- functions.sh
+++ functions.sh
@@ -234,13 +234,13 @@ status() {
     local rstatus=$(pm2status "${name}-relay" | awk '{print $4}')
     local cstatus=$(pm2status "${name}-core" | awk '{print $4}')
 
-    if [ "$rstatus" = "online" || "$cstatus" = "online" ]; then
+    if [[ "$rstatus" = "online" || "$cstatus" = "online" ]]; then
       echo -ne "relay: ${green}online${nc} "
     else
       echo -ne "relay: ${red}offline${nc} "
     fi
 
-    if [ "$fstatus" = "online" || "$cstatus" = "online" ]; then
+    if [[ "$fstatus" = "online" || "$cstatus" = "online" ]]; then
       echo -e "forger: ${green}online${nc}\n"
     else
       echo -e "forger: ${red}offline${nc}\n"
