10b979e81ca880c702bf3965d92c47a2cfdb9a1f
Dean van Dugteren <hello@dean.press>
2020-08-21 21:35:54 +0200

    chore: status

--- functions.sh
+++ functions.sh
@@ -233,13 +233,13 @@ status() {
     local fstatus=$(pm2status "${name}-forger" | awk '{print $4}')
     local rstatus=$(pm2status "${name}-relay" | awk '{print $4}')
 
-    if [ "$rstatus" = "online" ]; then
+    if [[ "$rstatus" = "online" || "$cstatus" = "online" ]]; then
       echo -ne "relay: ${green}online${nc} "
     else
       echo -ne "relay: ${red}offline${nc} "
     fi
 
-    if [ "$fstatus" = "online" ]; then
+    if [[ "$fstatus" = "online" || "$cstatus" = "online" ]]; then
       echo -e "forger: ${green}online${nc}\n"
     else
       echo -e "forger: ${red}offline${nc}\n"
